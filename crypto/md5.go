0cfecbe4147bfefd638f56f3fb8e7a7cac6b066f
AlexZ33 <775136985@qq.com>
2022-08-18 18:33:05 +0800

    update md5 crypto

--- crypto/md5.go
+++ crypto/md5.go
@@ -5,8 +5,27 @@ import (
 	"encoding/hex"
 )
 
-func ToMd5(data string) string {
+func ToMd5(data []byte) string {
 	h := md5.New()
-	h.Write([]byte(data))
+	h.Write(data)
 	return hex.EncodeToString(h.Sum(nil))
+}
+
+func Md5(str string) string {
+	return ToMd5([]byte(str))
+}
+
+/**
+* PasswordMD5 密码加密
+* @Description: create Account password MD5
+* @param passwd 密码
+* @return: string
+ */
+func PasswordMD5(passwd, salt string) string {
+	h := md5.New()
+	// 加盐： 后面增加一个无意义的字符串，防止破解
+	h.Write([]byte(passwd + salt + "x@.YuO-]"))
+	cipherStr := h.Sum(nil)
+	result := hex.EncodeToString(cipherStr)
+	return result
 }

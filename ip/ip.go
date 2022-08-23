b63836095464f64ff18b72312eca66fb2696335a
AlexZ33 <775136985@qq.com>
2022-08-23 11:38:41 +0800

    update ip util with GetRequestIP

--- ip/ip.go
+++ ip/ip.go
@@ -3,6 +3,8 @@ package ip
 import (
 	"github.com/AlexZ33/utils/errors"
 	"net"
+	"net/http"
+	"strings"
 )
 
 // GetLocalIP 获取本机IP地址
@@ -21,4 +23,22 @@ func GetLocalIP() (ip string, error error) {
 		}
 	}
 	return "", errors.New("获取本机IP地址失败")
+}
+
+// GetRequestIP 尽最大努力实现获取客户端 IP 的算法
+// 解析 X-Real-IP 和 X-Forwarded-For 以便于反向代理（nginx 或 haproxy）可以正常工作
+func GetRequestIP(r *http.Request) string {
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	if ip != "" {
+		return ip
+	}
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" {
+		return ip
+	}
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+		return ip
+	}
+	return ""
 }

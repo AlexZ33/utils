7dc393bdc7b487e18b3ff32d152f2ca70fe85ee2
AlexZ33 <775136985@qq.com>
2022-11-02 18:03:18 +0800

    Difference func returns the difference  between two string slices

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -322,3 +322,20 @@ func UniqueString(stringSlice []string) []string {
 	}
 	return list
 }
+
+// Difference returns the difference  between two string slices
+func Difference(a, b []string) (diff []string) {
+	m := make(map[string]bool)
+
+	for _, item := range b {
+		m[item] = true
+	}
+
+	for _, item := range a {
+		if _, ok := m[item]; !ok {
+			diff = append(diff, item)
+		}
+	}
+
+	return
+}

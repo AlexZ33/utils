3744627f28230d343b403297d8cb6206b058effc
AlexZ33 <775136985@qq.com>
2022-12-16 14:34:36 +0800

    add Sort

---
+++ algorithm/sort.go
@@ -0,0 +1,25 @@
+package algorithm
+
+import "sort"
+
+//Sort sorts values (in-place) with respect to the given comparator.
+func Sort(values []interface{}, comparator Comparator) {
+	sort.Sort(sortable{values, comparator})
+}
+
+type sortable struct {
+	values     []interface{}
+	comparator Comparator
+}
+
+func (s sortable) Len() int {
+	return len(s.values)
+}
+
+func (s sortable) Swap(i, j int) {
+	s.values[i], s.values[j] = s.values[j], s.values[i]
+}
+
+func (s sortable) Less(i, j int) bool {
+	return s.comparator(s.values[i], s.values[j]) < 0
+}
---
+++ containers/containers.go
@@ -0,0 +1,21 @@
+package containers
+
+import "github.com/AlexZ33/utils/algorithm"
+
+// Container is base interface that all data structures implement.
+type Container interface {
+	Empty() bool
+	Size() int
+	Clear()
+	Values() []interface{}
+	String() string
+}
+
+func GetSortedValues(container Container, comparator algorithm.Comparator) []interface{} {
+	values := container.Values()
+	if len(values) < 2 {
+		return values
+	}
+	algorithm.Sort(values, comparator)
+
+}
